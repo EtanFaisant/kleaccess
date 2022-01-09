ad59af4ed0793ecd6d8fca8bbac4308d7243b04d
Matheo Dourneau <matheo.dourneau@gmail.com>
2022-01-09 12:18:38 +0100

    modif css

--- credentials-table.js
+++ credentials-table.js
@@ -30,8 +30,8 @@ const tableModalTemplate = `
             {{#each this}}
             <tr>
                 <td scope="row"><i class="fas fa-globe"></i></td>
-                <td>{{sitename}}</td>
-                <td><a href='{{domain}}'>{{domain}}</a></td>
+                <td scope="row">{{sitename}}</td>
+                <td scope="row"><a href='{{domain}}'>{{domain}}</a></td>
                 <td>
                     <a id="btpopup" class="btn btn-outline-muted" role="button" onclick="confirmModify ({{@index}}); return false">
                     <span class="icon"><i class="fas fa-edit"></i></span>
